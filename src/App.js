7e031de284931343306dedf53dc549f4788bb262
ANANYA TRIPATHI <ananyatrip0802@gmail.com>
2022-09-16 00:33:02 +0530

    Create input task component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import Header from "./Component/Header";
 import Notes from "./Component/Notes";
 import { Footer } from "./Component/Footer";
+import InputTask from "./Component/Input"
 export default function App() {
   return (
     <div className="bg-slate-400">
       <Header />
+      <InputTask />
       <Notes />
       <Footer />
     </div>
